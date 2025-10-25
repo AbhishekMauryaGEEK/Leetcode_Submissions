5e0f938ec9877d559773b82d2aecdbb2e2ef2253
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-10-25 20:37:12 +0530

    Time: 0 ms (100%), Space: 7.7 MB (86.15%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -4,15 +4,11 @@ public:
         int sum = 0;
         int weeks = n / 7;
         int days = n % 7;
-
-        // Add money for complete weeks
         for (int i = 0; i < weeks; i++) {
             for (int j = 1; j <= 7; j++) {
                 sum += i + j;
             }
         }
-
-        // Add money for leftover days
         for (int j = 1; j <= days; j++) {
             sum += weeks + j;
         }
