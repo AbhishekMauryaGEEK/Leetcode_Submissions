447d1d6614dfe6b7f31d941792d550e24a705175
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-10-28 18:52:05 +0530

    Time: 6 ms (49.08%), Space: 22.1 MB (67.89%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -9,7 +9,7 @@ public:
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
                 if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
-                    ans+=2;
+                    ans++;
                 }
                 if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
                     ans++;
