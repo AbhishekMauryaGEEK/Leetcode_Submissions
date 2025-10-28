68f832934ec4d9ae8fd3bcf9fee394238ba91565
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2025-10-28 18:48:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
+++ 3354-make-array-elements-equal-to-zero/3354-make-array-elements-equal-to-zero.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-bool simulate(vector<int> nums, int start, int dir) {
-    int i = start;
-    while (i >= 0 && i < nums.size()) {
-        if (nums[i] == 0) {
-            i += dir;
-        } else {
-            nums[i]--;
-            dir *= -1;
-            i += dir;
+    int countValidSelections(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        int rightSum = sum;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == 0) {
+                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
+                    ans+=2;
+                }
+                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
+                    ans++;
+                }
+            } else {
+                leftSum += nums[i];
+                rightSum -= nums[i];
+            }
         }
+        return ans;
     }
-    for (int val : nums) {
-        if (val != 0) return false;
-    }
-    return true;
-}
-int countValidSelections(vector<int>& nums) {
-    int count = 0;
-    for (int i = 0; i < nums.size(); ++i) {
-        if (nums[i] == 0) {
-            if (simulate(nums, i, -1)) count++;
-            if (simulate(nums, i, 1)) count++;
-        }
-    }
-    return count;
-}
 };
