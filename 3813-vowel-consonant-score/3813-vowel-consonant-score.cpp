48019f5782ba8b6476148321cf2b9d68fd4afc20
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-01-23 14:20:53 +0530

    Time: 0 ms (100%), Space: 9 MB (90.6%) - LeetHub

--- 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
+++ 3813-vowel-consonant-score/3813-vowel-consonant-score.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
     int vowelConsonantScore(string s) {
-        int v=0,c=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u'){
+        int v = 0, c = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||
+                s[i] == 'u') {
                 v++;
+
+            } else if (isdigit(s[i])||s[i]==' ') {
+                continue;
             }
             else{
                 c++;
             }
         }
-        if(c==0){
+        if (c == 0) {
             return 0;
         }
-        
-        return v/c;
+
+        return v / c;
     }
 };
