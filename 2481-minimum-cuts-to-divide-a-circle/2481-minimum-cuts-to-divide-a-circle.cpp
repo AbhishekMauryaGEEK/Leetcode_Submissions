d9fb895706df288e9642f60b0167c4a387b43767
AbhishekMauryaGEEK <abhiotaku.am01@gmail.com>
2026-02-03 14:39:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2481-minimum-cuts-to-divide-a-circle/2481-minimum-cuts-to-divide-a-circle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfCuts(int n) {
+        int ans = 0;
+        if(n==1){
+            ans=0;
+        }
+        if (n % 2 == 0) {
+            ans = n / 2;
+        }
+        else  {
+            ans = n;
+        }
+        return ans;
+    }
+};
